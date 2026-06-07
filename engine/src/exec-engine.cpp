mwieder/hyperxtalk

commit d402be01b2613c357ad5c183c06920d477773eab

error code fix up

engine/src/executionerrors.h
@@ -2820,10 +2820,28 @@ enum Exec_errors
     EE_ICONDATAFOREXTENSION_BADEXT,
 
     // {EE-0922} iconDataForExtension: error evaluating size
-    EE_ICONDATAFOREXTENSION_BADSIZE
+    EE_ICONDATAFOREXTENSION_BADSIZE,
 
+    // {EE-0923} deleteCredential: bad service expression
+    EE_DELETECREDENTIAL_BADSERVICE,
+
+    // {EE-0924} deleteCredential: bad account expression
+    EE_DELETECREDENTIAL_BADACCOUNT,
+
+    // {EE-0925} retrieveCredential: bad service expression
+    EE_RETRIEVECREDENTIAL_BADSERVICE,
+
+    // {EE-0926} retrieveCredential: bad account expression
+    EE_RETRIEVECREDENTIAL_BADACCOUNT,
+
+    // {EE-0927} storeCredential: bad service expression
+    EE_STORECREDENTIAL_BADSERVICE,
+
+    // {EE-0928} storeCredential: bad account expression
+    EE_STORECREDENTIAL_BADACCOUNT,
+
+    // {EE-0929} storeCredential: bad secret expression
+    EE_STORECREDENTIAL_BADSECRET,
 };
-
-extern const char *MCexecutionerrors;
 
 #endif

engine/src/parseerrors.h
@@ -1810,10 +1810,10 @@ enum Parse_errors
 	
     // {PE-0582} send: can't send script in time
     PE_SEND_SCRIPTINTIME,
-    
-    // {PE-0583} fontLanguage: bad type expression
+
+    // {PE-0583} fontLanguage: bad type
     PE_FONTLANGUAGE_BADPARAM,
-    
+
     // {PE-0584} out of memory
     PE_OUTOFMEMORY,
 
@@ -1828,9 +1828,15 @@ enum Parse_errors
 
     // {PE-0588} iconDataForExtension: bad parameter
     PE_ICONDATAFOREXTENSION_BADPARAM,
+
+    // {PE-0589} deleteCredential: bad parameter expression
+    PE_DELETECREDENTIAL_BADPARAM,
+
+    // {PE-0590} retrieveCredential: bad parameter expression
+    PE_RETRIEVECREDENTIAL_BADPARAM,
+
+    // {PE-0591} storeCredential: bad parameter expression
+    PE_STORECREDENTIAL_BADPARAM,
 };
 
-extern const char *MCparsingerrors;
-
 #endif
-

commit 776183603fd02ea42366b95940a80018d2d35cbd

engine: restore extern declarations for MCexecutionerrors and MCparsingerrors
d402be01b accidentally dropped the extern const char *MCexecutionerrors and extern const char *MCparsingerrors declarations from the bottoms of executionerrors.h and parseerrors.h when adding the credential error codes. Without them, any translation unit that includes these headers (such as exec-engine.cpp) cannot see the declarations for the variables defined in encodederrors.cpp (or the empty-string stubs in the mode files), causing an "undeclared identifier" build error. Restores both declarations to their original positions.

engine/src/exec-engine.cpp
@@ -1562,10 +1562,6 @@ void MCEngineGetNumberFormat(MCExecContext& ctxt, MCEngineNumberFormat& r_format
 
 ///////////////////////////////////////////////////////////////////////////////
 
-// Defined in encodederrors.cpp (community/dev builds) or as "" in mode_*.cpp
-extern const char *MCexecutionerrors;
-extern const char *MCparsingerrors;
-
 void MCEngineGetScriptExecutionErrors(MCExecContext& ctxt, MCStringRef &r_value)
 {
 	if (MCStringCreateWithCString(MCexecutionerrors, r_value))

engine/src/executionerrors.h
@@ -2844,4 +2844,6 @@ enum Exec_errors
     EE_STORECREDENTIAL_BADSECRET,
 };
 
+extern const char *MCexecutionerrors;
+
 #endif

engine/src/parseerrors.h
@@ -1839,4 +1839,6 @@ enum Parse_errors
     PE_STORECREDENTIAL_BADPARAM,
 };
 
+extern const char *MCparsingerrors;
+
 #endif